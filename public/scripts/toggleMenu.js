9767b806d1883e371740cc7f65977ef8c428f107
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2024-09-24 20:42:22 +0200

    fix menu on small devices to work correctly if one of the links are clicked

--- public/scripts/toggleMenu.js
+++ public/scripts/toggleMenu.js
@@ -2,9 +2,17 @@ const burger = document.getElementsByClassName('burger-menu')[0];
 const menu = document.getElementById('main-menu');
 const burgerIcon = document.getElementById('burger-icon');
 
-burger.addEventListener('click', () => {
+const setMenuState = () => {
     const isExpanded = burger.getAttribute('aria-expanded') === 'true';
     burger.setAttribute('aria-expanded', !isExpanded);
     menu.setAttribute('aria-hidden', isExpanded);
     isExpanded ? burgerIcon.className = 'fas fa-bars' : burgerIcon.className = 'fas fa-times'
+}
+burger.addEventListener('click', () => {
+    setMenuState();
 });
+Array.from(menu.children).forEach(child => {
+    child.addEventListener('click', () => setMenuState());
+})
+
+
